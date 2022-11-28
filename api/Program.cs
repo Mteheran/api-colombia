274cbe45aae76638bf63c0505b56f1871bcdb4ba
Mario Andres Botero Florez <mariobot113@hotmail.com>
2022-11-28 11:26:38 -0500

    Add Swagger

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.Run();

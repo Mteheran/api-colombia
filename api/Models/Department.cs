0265988f3062c7510907c5ff886b16daa6e18413
VeronicaGuaman <veronica_guaman1997@outlook.es>
2023-02-01 19:57:41 -0500

    Update Cities and departments

--- api/Models/Department.cs
+++ api/Models/Department.cs
@@ -6,7 +6,7 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public int? CityCapitalId { get; set; }
-        public string[]? Municipalities { get; set; }
+        public int? Municipalities { get; set; }
         public float Surface { get; set; }
         public float? Population { get; set; }
         public string? PhonePrefix { get; set; }

a4b877983848150edd2acae1f3d9b5abd16e67b9
AC/Dev <angelocardona85@gmail.com>
2023-04-23 09:54:14 -0500

    adding produce to NaturalAreaRoutes

--- api/Routes/NaturalAreaRoutes.cs
+++ api/Routes/NaturalAreaRoutes.cs
@@ -18,6 +18,7 @@ namespace api.Routes
             {
                 return Results.Ok(await db.NaturalAreas.ToListAsync());
             })
+            .Produces<List<Map>>(200)
             .WithMetadata(new SwaggerOperationAttribute(
                 summary: NaturalAreaEndpoint.MESSAGE_LIST_SUMMARY,
                 description: NaturalAreaEndpoint.MESSAGE_LIST_DESCRIPTION
@@ -42,12 +43,13 @@ namespace api.Routes
 
                 return Results.Ok(naturalArea);
             })
-          .WithMetadata(new SwaggerOperationAttribute(
+            .Produces<NaturalArea?>(200)
+            .WithMetadata(new SwaggerOperationAttribute(
               summary: NaturalAreaEndpoint.MESSAGE_BYID_SUMMARY,
               description: NaturalAreaEndpoint.MESSAGE_BYID_DESCRIPTION));
         
 
-          app.MapGet($"{API_NATURALAREA_ROUTE_COMPLETE}/name/{{name}}", async (string name, DBContext db) =>
+            app.MapGet($"{API_NATURALAREA_ROUTE_COMPLETE}/name/{{name}}", async (string name, DBContext db) =>
             {
                 var naturalAreas = await db.NaturalAreas
                                             .Include(p=> p.CategoryNaturalArea).IgnoreAutoIncludes()
@@ -61,6 +63,7 @@ namespace api.Routes
 
                 return Results.Ok(naturalAreas);
             })
+            .Produces<List<NaturalArea>?>(200)
             .WithMetadata(new SwaggerOperationAttribute(
                 summary: NaturalAreaEndpoint.MESSAGE_BYNAME_SUMMARY,
                 description: NaturalAreaEndpoint.MESSAGE_BYNAME_DESCRIPTION
@@ -70,23 +73,21 @@ namespace api.Routes
             {
                 string wellFormedKeyword = keyword.Trim().ToUpper().Normalize();
                 var naturalAreas = db.NaturalAreas.ToList();
-
-                var departments = Functions.FilterObjectListPropertiesByKeyword<NaturalArea>(naturalAreas, wellFormedKeyword);
-
-                if (departments.Count == 0)
+                var naturalAreasFiltered = Functions.FilterObjectListPropertiesByKeyword<NaturalArea>(naturalAreas, wellFormedKeyword);
+                if (naturalAreasFiltered.Count == 0)
                 {
                     return Results.NotFound();
                 }
 
-                return Results.Ok(departments);
+                return Results.Ok(naturalAreasFiltered);
             })
+            .Produces<List<NaturalArea>?>(200)
             .WithMetadata(new SwaggerOperationAttribute(
                 summary: NaturalAreaEndpoint.MESSAGE_SEARCH_SUMMARY, 
                 description: NaturalAreaEndpoint.MESSAGE_SEARCH_DESCRIPTION
                 ));
 
-            app.MapGet($"{API_NATURALAREA_ROUTE_COMPLETE}/pagedList",
-                  async ([AsParameters] PaginationModel pagination, DBContext db) =>
+            app.MapGet($"{API_NATURALAREA_ROUTE_COMPLETE}/pagedList", async ([AsParameters] PaginationModel pagination, DBContext db) =>
                   {
 
                       if (pagination.Page <= 0 || pagination.PageSize <= 0)
@@ -95,7 +96,6 @@ namespace api.Routes
                       }
 
                       var naturalAreaPaged = db.NaturalAreas.Skip((pagination.Page - 1) * pagination.PageSize).Take(pagination.PageSize);
-
                       if (!await naturalAreaPaged?.AnyAsync())
                       {
                           return Results.NotFound();
@@ -111,7 +111,8 @@ namespace api.Routes
 
                       return Results.Ok(paginationResponse);
                   })
-         .WithMetadata(new SwaggerOperationAttribute(
+            .Produces<PaginationResponseModel<NaturalArea>?>(200)
+            .WithMetadata(new SwaggerOperationAttribute(
              summary: NaturalAreaEndpoint.MESSAGE_PAGEDLIST_SUMMARY,
               description: NaturalAreaEndpoint.MESSAGE_PAGEDLIST_DESCRIPTION
               ));

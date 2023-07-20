a795b7efdc5a48e9fb46a131551ff530563de7fe
Miguel Teheran <miguelteheran@gmail.com>
2023-07-19 21:59:41 -0500

    adding invasive specie structure

---
+++ api/Models/InvasiveSpecie.cs
@@ -0,0 +1,19 @@
+﻿namespace api.Models
+{
+    public class InvasiveSpecie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ScientificName { get; set; }
+        public string CommonNames { get; set; }
+        public string Impact { get; set; }
+        public string Manage { get; set; }
+        public RiskLevel RiskLevel { get; set; }
+        public string UrlImage { get; set; }
+    }
+
+    public enum RiskLevel
+    {
+        Low, Medium, High
+    }
+}

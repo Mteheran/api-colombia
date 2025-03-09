09a2b9d94a4e5cb41b662440f3f17330303ea6e5
Gael Abreu <gabreu@farmaplan.com.do>
2025-03-09 10:44:34 -0400

    Adding Natural Area Integration Test.

--- api.Tests/ApiRoutesTests/NaturalAreaApiIntegrationTests.cs
+++ api.Tests/ApiRoutesTests/NaturalAreaApiIntegrationTests.cs
@@ -1,111 +1,116 @@
-// using System.Net.Http;
-// using System.Threading.Tasks;
-// using Xunit;
-// using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-// public class NaturalAreaApiIntegrationTests : IClassFixture<CustomWebApplicationFactory>
-// {
-//     private readonly HttpClient _client;
+public class NaturalAreaApiIntegrationTests : IClassFixture<CustomWebApplicationFactory> , IDisposable
+{
+    private readonly HttpClient _client;
 
-//     public NaturalAreaApiIntegrationTests(CustomWebApplicationFactory factory)
-//     {
-//         _client = factory.CreateClient();  
-//     }
+    public NaturalAreaApiIntegrationTests(CustomWebApplicationFactory factory)
+    {
+       _client = new CustomWebApplicationFactory().CreateClient(); 
+    }
 
-//     [Fact]
-//     public async Task GetNaturalAreas_ReturnsOkWithExpectedData()
-//     {
-//         var response = await _client.GetAsync("/api/v1/NaturalArea/");
+    public void Dispose()
+    {
+        _client.Dispose();
+    }
+
+    [Fact]
+    public async Task GetNaturalAreas_ReturnsOkWithExpectedData()
+    {
+        var response = await _client.GetAsync("/api/v1/NaturalArea/");
         
-//         response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
         
-//         var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync();
         
-//         Assert.NotNull(result);    
-//         Assert.False(string.IsNullOrEmpty(result));  
-//     }
+        Assert.NotNull(result);    
+        Assert.False(string.IsNullOrEmpty(result));  
+    }
 
-//     [Fact]
-//     public async Task GetNaturalAreaById_ReturnsOkWithNaturalAreaData()
-//     {
-//         int naturalAreaId = 1;  
-//         var response = await _client.GetAsync($"/api/v1/NaturalArea/{naturalAreaId}");
+    [Fact]
+    public async Task GetNaturalAreaById_ReturnsOkWithNaturalAreaData()
+    {
+        int naturalAreaId = 1;  
+        var response = await _client.GetAsync($"/api/v1/NaturalArea/{naturalAreaId}");
 
-//         response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
 
-//         var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync();
         
-//         Assert.NotNull(result);
-//         Assert.Contains("Id", result);  
-//     }
+        Assert.NotNull(result);
+        Assert.Contains("id", result);  
+    }
 
-//     [Fact]
-//     public async Task GetNaturalAreaByName_ReturnsOkWithNaturalAreaData()
-//     {
-//         string naturalAreaName = "Sample Natural Area";  
-//         var response = await _client.GetAsync($"/api/v1/NaturalArea/name/{naturalAreaName}");
+    [Fact]
+    public async Task GetNaturalAreaByName_ReturnsOkWithNaturalAreaData()
+    {
+        string naturalAreaName = "Parque Arví";  
+        var response = await _client.GetAsync($"/api/v1/NaturalArea/name/{naturalAreaName}");
 
-//         response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
 
-//         var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync();
         
-//         Assert.NotNull(result);
-//         Assert.Contains("Name", result);  
-//     }
+        Assert.NotNull(result);
+        Assert.Contains("name", result);  
+    }
 
-//     [Fact]
-//     public async Task SearchNaturalAreas_ReturnsOkWithFilteredNaturalAreasData()
-//     {
-//         string keyword = "Sample";  
-//         var response = await _client.GetAsync($"/api/v1/NaturalArea/search/{keyword}");
+    [Fact]
+    public async Task SearchNaturalAreas_ReturnsOkWithFilteredNaturalAreasData()
+    {
+        string keyword = "Parque";  
+        var response = await _client.GetAsync($"/api/v1/NaturalArea/search/{keyword}");
 
-//         response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
 
-//         var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync();
         
-//         Assert.NotNull(result);
-//         Assert.Contains("NaturalAreas", result);  
-//     }
+        Assert.NotNull(result);
+        Assert.Contains("categoryNatural", result);  
+    }
 
-//     [Fact]
-//     public async Task GetPagedNaturalAreas_ReturnsOkWithPagedNaturalAreasData()
-//     {
-//         string sortBy = "Name"; 
-//         string sortDirection = "asc";  
-//         int page = 1;
-//         int pageSize = 10;
-//         var response = await _client.GetAsync($"/api/v1/NaturalArea/pagedList?page={page}&pageSize={pageSize}&sortBy={sortBy}&sortDirection={sortDirection}");
+    [Fact]
+    public async Task GetPagedNaturalAreas_ReturnsOkWithPagedNaturalAreasData()
+    {
+        string sortBy = "Name"; 
+        string sortDirection = "asc";  
+        int page = 1;
+        int pageSize = 10;
+        var response = await _client.GetAsync($"/api/v1/NaturalArea/pagedList?page={page}&pageSize={pageSize}&sortBy={sortBy}&sortDirection={sortDirection}");
 
-//         response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
 
-//         var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync();
         
-//         Assert.NotNull(result);
-//         Assert.Contains("Data", result);  
-//     }
+        Assert.NotNull(result);
+        Assert.Contains("pageSize", result);  
+    }
 
-//     [Fact]
-//     public async Task GetNaturalAreasWithSorting_ReturnsOkWithSortedData()
-//     {
-//         string sortBy = "Name"; 
-//         string sortDirection = "asc";  
+    [Fact]
+    public async Task GetNaturalAreasWithSorting_ReturnsOkWithSortedData()
+    {
+        string sortBy = "id"; 
+        string sortDirection = "asc";  
 
-//         var response = await _client.GetAsync($"/api/v1/NaturalArea?sortBy={sortBy}&sortDirection={sortDirection}");
+        var response = await _client.GetAsync($"/api/v1/NaturalArea?sortBy={sortBy}&sortDirection={sortDirection}");
 
-//         response.EnsureSuccessStatusCode();
+        response.EnsureSuccessStatusCode();
 
-//         var result = await response.Content.ReadAsStringAsync();
+        var result = await response.Content.ReadAsStringAsync();
         
-//         Assert.NotNull(result);
-//         Assert.Contains("Name", result);  
-//     }
+        Assert.NotNull(result);
+        Assert.Contains("id", result);  
+    }
 
-//     [Fact]
-//     public async Task GetNaturalAreaById_ReturnsNotFoundWhenIdIsInvalid()
-//     {
-//         int invalidId = -1;  
-//         var response = await _client.GetAsync($"/api/v1/NaturalArea/{invalidId}");
+    [Fact]
+    public async Task GetNaturalAreaById_ReturnsNotFoundWhenIdIsInvalid()
+    {
+        int invalidId = -1;  
+        var response = await _client.GetAsync($"/api/v1/NaturalArea/{invalidId}");
 
-//         Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
-//     }
-// }
+        Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}

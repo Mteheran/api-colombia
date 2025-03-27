c5df2b673bd2f5cd3820c94424d1bae6167dadf0
Miguel Teheran <miguelteheran@gmail.com>
2025-03-27 17:38:53 -0500

    Refactor Traditional Fair and Festival API integration tests by removing unused namespaces

--- api.Tests/ApiRoutesTests/TraditionalFairAndFestivalApiIntegrationTests.cs
+++ api.Tests/ApiRoutesTests/TraditionalFairAndFestivalApiIntegrationTests.cs
@@ -1,10 +1,5 @@
-using System.Net.Http;
-using System.Threading.Tasks;
-using Xunit;
-using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http.Json;
 using api.Models;
-using api.Migrations;
 using api.Utils;
 
 public class TraditionalFairAndFestivalApiIntegrationTests : IClassFixture<CustomWebApplicationFactory>
